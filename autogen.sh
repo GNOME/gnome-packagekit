c2686bc79afbfb8a8f0bf29f6c829795ff3a6632
Richard Hughes <richard@hughsie.com>
2007-08-23 15:56:28 +0100

    initial

---
+++ autogen.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME=packagekit-gnome
+
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/autogen.sh \
+  && test -d $srcdir/src \
+  && test -f $srcdir/src/gpm-main.c) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common!"
+    exit 1
+}
+
+REQUIRED_AUTOMAKE_VERSION=1.7 GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
