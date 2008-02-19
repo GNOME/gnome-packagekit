6db1c6e07646ce4c33879cbc3e5e6289d8db846f
Richard Hughes <richard@hughsie.com>
2008-02-19 00:58:18 +0000

    renamed configure.in to configure.ac

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME=gnome-packagekit
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -f $srcdir/autogen.sh \
   && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
