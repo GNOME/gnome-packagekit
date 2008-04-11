8eceda9d018508aea02c85f46324e90b81967389
Richard Hughes <richard@hughsie.com>
2008-04-11 22:38:31 +0100

    add another method to provide a file, and rename the client tools to be clearer

--- libpackagekit-gnome/gpk-client.h
+++ libpackagekit-gnome/gpk-client.h
@@ -55,6 +55,9 @@ GpkClient	*gpk_client_new				(void);
 gboolean	 gpk_client_install_local_file		(GpkClient	*gclient,
 							 const gchar	*file_rel)
 							 G_GNUC_WARN_UNUSED_RESULT;
+gboolean	 gpk_client_install_provide_file	(GpkClient	*gclient,
+							 const gchar	*full_path)
+							 G_GNUC_WARN_UNUSED_RESULT;
 gboolean	 gpk_client_install_package_name	(GpkClient	*gclient,
 							 const gchar	*package)
 							 G_GNUC_WARN_UNUSED_RESULT;
---
+++ src/gpk-install-local-file.c
@@ -0,0 +1,88 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2007-2008 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+#include <locale.h>
+
+#include <pk-debug.h>
+
+#include "gpk-common.h"
+#include "gpk-client.h"
+
+/**
+ * main:
+ **/
+int
+main (int argc, char *argv[])
+{
+	GOptionContext *context;
+	gboolean ret;
+	gboolean verbose = FALSE;
+	GError *error;
+	GpkClient *gclient;
+
+	const GOptionEntry options[] = {
+		{ "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose,
+		  N_("Show extra debugging information"), NULL },
+		{ NULL}
+	};
+
+	setlocale (LC_ALL, "");
+
+	bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
+
+	if (! g_thread_supported ()) {
+		g_thread_init (NULL);
+	}
+	g_type_init ();
+
+	g_set_application_name (_("PackageKit File Installer"));
+	context = g_option_context_new ("gpk-install-file");
+	g_option_context_set_summary (context, _("PackageKit File Installer"));
+	g_option_context_add_main_entries (context, options, NULL);
+	g_option_context_parse (context, &argc, &argv, NULL);
+	g_option_context_free (context);
+
+	pk_debug_init (verbose);
+	gtk_init (&argc, &argv);
+
+	if (argc < 2) {
+		gpk_error_modal_dialog (_("Failed to install"), _("You need to specify a file to install"));
+		return 1;
+	}
+	if (argc > 2) {
+		gpk_error_modal_dialog (_("Failed to install files"), _("You can only specify one file to install"));
+		return 1;
+	}
+
+	error = NULL;
+	gclient = gpk_client_new ();
+	ret = gpk_client_install_local_file (gclient, argv[1]);
+	g_object_unref (gclient);
+
+	return !ret;
+}
