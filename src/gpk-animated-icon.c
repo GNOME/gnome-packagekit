ec9df53ba746cc22d68aafd332ffde874153b72f
Richard Hughes <richard@hughsie.com>
2008-05-19 19:11:02 +0100

    add a simple GtkImage superclass to handle animation from tiles

---
+++ src/gpk-animated-icon.c
@@ -0,0 +1,198 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2008 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+#include <pk-debug.h>
+
+#include "gpk-animated-icon.h"
+
+G_DEFINE_TYPE (GpkAnimatedIcon, gpk_animated_icon, GTK_TYPE_IMAGE)
+
+static gpointer parent_class = NULL;
+
+/**
+ * gpk_animated_icon_set_filename_tile:
+ **/
+gboolean
+gpk_animated_icon_set_filename_tile (GpkAnimatedIcon *icon, GtkIconSize size, const gchar *name)
+{
+	gint w, h;
+	gint rows, cols;
+	gint r, c, i;
+	GdkPixbuf *pixbuf;
+
+	g_return_val_if_fail (GPK_IS_ANIMATED_ICON (icon), FALSE);
+
+	if (icon->frames != NULL) {
+		pk_debug ("already set, not supported");
+		return FALSE;
+	}
+
+	pk_debug ("loading from %s", name);
+	gtk_icon_size_lookup (size, &w, &h);
+
+	pixbuf = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (), name, w, 0, NULL);
+	/* can't load from gnome-icon-theme */
+	if (pixbuf == NULL) {
+		pk_warning ("can't load %s", name);
+		return FALSE;
+	}
+
+	cols = gdk_pixbuf_get_width (pixbuf) / w;
+	rows = gdk_pixbuf_get_height (pixbuf) / h;
+
+	icon->number_frames = rows * cols;
+	icon->frames = g_new (GdkPixbuf*, icon->number_frames);
+
+	for (i = 0, r = 0; r < rows; r++)
+		for (c = 0; c < cols; c++, i++) {
+		icon->frames[i] = gdk_pixbuf_new_subpixbuf (pixbuf, c * w, r * h, w, h);
+	}
+
+	g_object_unref (pixbuf);
+
+	return TRUE;
+}
+
+/**
+ * gpk_animated_icon_set_frame_delay:
+ **/
+gboolean
+gpk_animated_icon_set_frame_delay (GpkAnimatedIcon *icon, guint delay_ms)
+{
+	g_return_val_if_fail (GPK_IS_ANIMATED_ICON (icon), FALSE);
+
+	pk_debug ("frame delay set to %ims", delay_ms);
+	icon->frame_delay = delay_ms;
+	return TRUE;
+}
+
+/**
+ * gpk_animated_icon_update:
+ **/
+static gboolean
+gpk_animated_icon_update (GpkAnimatedIcon *icon)
+{
+	/* have we loaded a file */
+	if (icon->frames == NULL) {
+		pk_warning ("no frames to process");
+		return FALSE;
+	}
+
+	/* set new */
+	gtk_image_set_from_pixbuf (GTK_IMAGE (icon), icon->frames[icon->frame_counter]);
+
+	/* advance counter, wrapping around */
+	icon->frame_counter = (icon->frame_counter + 1) % icon->number_frames;
+
+	return TRUE;
+}
+
+
+/**
+ * gpk_animated_icon_enable_animation:
+ **/
+gboolean
+gpk_animated_icon_enable_animation (GpkAnimatedIcon *icon, gboolean enabled)
+{
+	g_return_val_if_fail (GPK_IS_ANIMATED_ICON (icon), FALSE);
+
+	if (!enabled) {
+		if (icon->animation_id == 0) {
+			pk_debug ("ignoring stop on stopped icon");
+			return FALSE;
+		}
+
+		g_source_remove (icon->animation_id);
+		icon->animation_id = 0;
+		return TRUE;
+	}
+
+	/* don't double queue */
+	if (icon->animation_id != 0) {
+		pk_debug ("ignoring start on started icon");
+		return FALSE;
+	}
+
+	/* start */
+	icon->frame_counter = 0;
+	icon->animation_id = g_timeout_add (icon->frame_delay, (GSourceFunc) gpk_animated_icon_update, icon);
+	gpk_animated_icon_update (icon);
+	return TRUE;
+}
+
+/**
+ * gpk_image_finalize:
+ * @object: The object to finalize
+ **/
+static void
+gpk_image_finalize (GObject *object)
+{
+	GpkAnimatedIcon *icon;
+	icon = GPK_ANIMATED_ICON (object);
+
+	/* avoid going pop after unref when spinning */
+	if (icon->animation_id != 0) {
+		g_source_remove (icon->animation_id);
+	}
+
+
+	G_OBJECT_CLASS (parent_class)->finalize (object);
+}
+
+static void
+gpk_animated_icon_class_init (GpkAnimatedIconClass *class)
+{
+	GtkImageClass *image_class;
+	GObjectClass *object_class = G_OBJECT_CLASS (class);
+	object_class->finalize = gpk_image_finalize;
+
+	parent_class = g_type_class_peek_parent (class);
+	image_class = GTK_IMAGE_CLASS (class);
+}
+
+/**
+ * gpk_animated_icon_init:
+ **/
+static void
+gpk_animated_icon_init (GpkAnimatedIcon *icon)
+{
+	g_return_if_fail (GPK_IS_ANIMATED_ICON (icon));
+	icon->frames = NULL;
+	icon->animation_id = 0;
+	icon->frame_counter = 0;
+	icon->number_frames = 0;
+	icon->frame_delay = 200;
+}
+
+/**
+ * gpk_animated_icon_new:
+ **/
+GtkWidget *
+gpk_animated_icon_new (void)
+{
+	return g_object_new (GPK_TYPE_ANIMATED_ICON, NULL);
+}
+
---
+++ src/gpk-animated-icon.h
@@ -0,0 +1,68 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2008 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef GPK_ANIMATED_ICON_H
+#define GPK_ANIMATED_ICON_H
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#define GPK_TYPE_ANIMATED_ICON			(gpk_animated_icon_get_type())
+#define GPK_ANIMATED_ICON(obj)			(G_TYPE_CHECK_INSTANCE_CAST((obj), GPK_TYPE_ANIMATED_ICON, GpkAnimatedIcon))
+#define GPK_ANIMATED_ICON_CLASS(cls)		(G_TYPE_CHECK_CLASS_CAST((cls), GPK_TYPE_ANIMATED_ICON, GpkAnimatedIconClass))
+#define GPK_IS_ANIMATED_ICON(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), GPK_TYPE_ANIMATED_ICON))
+#define GPK_IS_ANIMATED_ICON_CLASS(cls)		(G_TYPE_CHECK_CLASS_TYPE((cls), GPK_TYPE_ANIMATED_ICON))
+#define GPK_ANIMATED_ICON_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS((obj), GPK_TYPE_ANIMATED_ICON, GpkAnimatedIconClass))
+
+G_BEGIN_DECLS
+
+typedef struct _GpkAnimatedIcon			GpkAnimatedIcon;
+typedef struct _GpkAnimatedIconClass		GpkAnimatedIconClass;
+
+struct _GpkAnimatedIcon
+{
+	GtkImage		 parent;
+	guint			 animation_id;
+	guint			 frame_counter;
+	guint			 number_frames;
+	guint			 frame_delay;
+	GdkPixbuf		**frames;
+};
+
+struct _GpkAnimatedIconClass
+{
+	GtkImageClass		 parent_class;
+};
+
+GType		 gpk_animated_icon_get_type		(void) G_GNUC_CONST;
+GtkWidget	*gpk_animated_icon_new			(void);
+gboolean	 gpk_animated_icon_set_filename_tile	(GpkAnimatedIcon	*icon,
+							 GtkIconSize		 size,
+							 const gchar		*name);
+gboolean	 gpk_animated_icon_set_frame_delay	(GpkAnimatedIcon	*icon,
+							 guint			 delay_ms);
+gboolean	 gpk_animated_icon_enable_animation	(GpkAnimatedIcon	*icon,
+							 gboolean		 enabled);
+
+G_END_DECLS
+
+#endif /* GPK_ANIMATED_ICON_H */
+
