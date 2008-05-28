119a0c399f76f09484eaa938af984156843098d2
Richard Hughes <richard@hughsie.com>
2008-05-28 11:14:42 +0100

    bugfix: disable the animation when we do a static image

--- src/gpk-common.c
+++ src/gpk-common.c
@@ -1376,6 +1376,7 @@ gpk_set_animated_icon_from_status (GpkAnimatedIcon *icon, PkStatusEnum status, G
 		gpk_animated_icon_set_filename_tile (icon, size, name);
 		gpk_animated_icon_enable_animation (icon, TRUE);
 	} else {
+		gpk_animated_icon_enable_animation (icon, FALSE);
 		gtk_image_set_from_icon_name (GTK_IMAGE (icon), name, size);
 	}
 
