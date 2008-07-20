e15dc62e34ba4f7ad8e60efdea12ba6a03c58395
Richard Hughes <richard@hughsie.com>
2008-07-20 18:57:12 +0100

    feature: populate the autocompletion box in gpk-application if the package list exists and the gconf key is set

--- src/gpk-application.c
+++ src/gpk-application.c
@@ -1925,6 +1925,8 @@ gpk_application_entry_text_icon_pressed_cb (SexyIconEntry *entry, gint icon_pos,
 			1, gtk_get_current_event_time());
 }
 
+#define PK_PACKAGE_LIST_LOCATION	"/var/lib/PackageKit/package-list.txt"
+
 /**
  * gpk_application_create_completion_model:
  *
@@ -1933,22 +1935,42 @@ gpk_application_entry_text_icon_pressed_cb (SexyIconEntry *entry, gint icon_pos,
 static GtkTreeModel *
 gpk_application_create_completion_model (void)
 {
+	PkPackageList *list;
+	guint i;
+	guint length;
+	gboolean ret;
+	const PkPackageObj *obj;
+	GHashTable *hash;
+	gpointer data;
 	GtkListStore *store;
 	GtkTreeIter iter;
 
 	store = gtk_list_store_new (1, G_TYPE_STRING);
+	hash = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
+	list = pk_package_list_new ();
+	ret = pk_package_list_add_file (list, PK_PACKAGE_LIST_LOCATION);
+	if (!ret) {
+		pk_warning ("no package list, try running pk-generate-package-list as root");
+		return NULL;
+	}
 
-	/* append one word */
-	gtk_list_store_append (store, &iter);
-	gtk_list_store_set (store, &iter, 0, "gnome-power-manager", -1);
-
-	/* append another word */
-	gtk_list_store_append (store, &iter);
-	gtk_list_store_set (store, &iter, 0, "gnome-screensaver", -1);
-
-	/* and another word */
-	gtk_list_store_append (store, &iter);
-	gtk_list_store_set (store, &iter, 0, "hal", -1);
+	length = pk_package_list_get_size (list);
+	for (i=0; i<length; i++) {
+		obj = pk_package_list_get_obj (list, i);
+		if (obj == NULL || obj->id == NULL || obj->id->name == NULL) {
+			pk_warning ("obj invalid!");
+			break;
+		}
+		data = g_hash_table_lookup (hash, (gpointer) obj->id->name);
+		if (data == NULL) {
+			/* append just the name */
+			g_hash_table_insert (hash, g_strdup (obj->id->name), GINT_TO_POINTER (1));
+			gtk_list_store_append (store, &iter);
+			gtk_list_store_set (store, &iter, 0, obj->id->name, -1);
+		}
+	}
+	g_hash_table_unref (hash);
+	g_object_unref (list);
 
 	return GTK_TREE_MODEL (store);
 }
@@ -2887,12 +2909,14 @@ gpk_application_init (GpkApplication *application)
 
 		/* create a tree model and use it as the completion model */
 		completion_model = gpk_application_create_completion_model ();
-		gtk_entry_completion_set_model (completion, completion_model);
-		g_object_unref (completion_model);
+		if (completion_model != NULL) {
+			gtk_entry_completion_set_model (completion, completion_model);
+			g_object_unref (completion_model);
 
-		/* use model column 0 as the text column */
-		gtk_entry_completion_set_text_column (completion, 0);
-		gtk_entry_completion_set_inline_completion (completion, TRUE);
+			/* use model column 0 as the text column */
+			gtk_entry_completion_set_text_column (completion, 0);
+			gtk_entry_completion_set_inline_completion (completion, TRUE);
+		}
 	}
 
 	/* set focus on entry text */
