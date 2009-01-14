29e0f9f87a32fd60a9c73f4b32cb11ed5284d8df
Richard Hughes <richard@hughsie.com>
2009-01-14 13:16:25 +0000

    bugfix: allow pattern matches in the banned firmware list

--- src/gpk-firmware.c
+++ src/gpk-firmware.c
@@ -41,7 +41,6 @@
 
 #include "egg-debug.h"
 #include "egg-string.h"
-#include "egg-string-list.h"
 
 #include "gpk-client.h"
 #include "gpk-common.h"
@@ -52,12 +51,12 @@ static void     gpk_firmware_finalize	(GObject	  *object);
 #define GPK_FIRMWARE_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), GPK_TYPE_FIRMWARE, GpkFirmwarePrivate))
 #define GPK_FIRMWARE_MISSING_DIR		"/dev/.udev/firmware-missing"
 #define GPK_FIRMWARE_LOADING_DIR		"/lib/firmware"
-#define GPK_FIRMWARE_LOGIN_DELAY	60 /* seconds */
+#define GPK_FIRMWARE_LOGIN_DELAY		60 /* seconds */
 
 struct GpkFirmwarePrivate
 {
-	EggStrList		*array_found;
-	EggStrList		*array_requested;
+	GPtrArray		*array_found;
+	GPtrArray		*array_requested;
 	GConfClient		*gconf_client;
 };
 
@@ -81,7 +80,7 @@ gpk_firmware_install_file (GpkFirmware *firmware)
 
 	/* try to install each firmware file */
 	for (i=0; i<array->len; i++) {
-		filename = egg_str_list_index (array, i);
+		filename = g_ptr_array_index (array, i);
 		gpk_client_set_interaction (gclient, GPK_CLIENT_INTERACT_WARNING_PROGRESS);
 		ret = gpk_client_install_provide_file (gclient, filename, &error);
 		if (!ret) {
@@ -159,10 +158,11 @@ out:
 static gboolean
 gpk_firmware_timeout_cb (gpointer data)
 {
-	guint i;
+	guint i, j;
 	gboolean ret;
 	const gchar *filename;
 	const gchar *message;
+	gchar *duplicate;
 	GpkFirmware *firmware = GPK_FIRMWARE (data);
 	NotifyNotification *notification;
 	GPtrArray *array;
@@ -174,11 +174,11 @@ gpk_firmware_timeout_cb (gpointer data)
 	/* try to find each firmware file in an available package */
 	array = firmware->priv->array_requested;
 	for (i=0; i<array->len; i++) {
-		filename = egg_str_list_index (array, i);
+		filename = g_ptr_array_index (array, i);
 		/* save to new array if we found one package for this file */
 		ret = gpk_firmware_check_available (firmware, filename);
 		if (ret)
-			egg_str_list_add (firmware->priv->array_found, filename);
+			g_ptr_array_add (firmware->priv->array_found, g_strdup (filename));
 	}
 
 	/* nothing to do */
@@ -189,11 +189,22 @@ gpk_firmware_timeout_cb (gpointer data)
 	}
 
 	/* check we don't want the same package more than once */
-	egg_str_list_remove_duplicate (array);
+	for (i=0; i<array->len; i++) {
+		for (j=0; j<array->len; j++) {
+			duplicate = g_ptr_array_index (array, j);
+			if (i != j && egg_strequal (g_ptr_array_index (array, i), duplicate)) {
+				g_free (duplicate);
+				g_ptr_array_remove_index_fast (array, j);
+			}
+		}
+	}
 
 	/* debugging */
 	egg_debug ("need to install:");
-	egg_str_list_print (array);
+	for (i=0; i<array->len; i++) {
+		filename = g_ptr_array_index (array, i);
+		egg_debug ("%s", filename);
+	}
 
 	/* TRANSLATORS: we need another package to keep udev quiet */
 	message = _("Additional firmware is required to make hardware in this computer function correctly.");
@@ -226,7 +237,9 @@ gpk_firmware_remove_banned (GpkFirmware *firmware, GPtrArray *array)
 {
 	gchar *banned_str;
 	gchar **banned = NULL;
-	EggStrList *banned_list = NULL;
+	gchar *filename;
+	guint i, j;
+	gboolean ret;
 
 	/* get from gconf */
 	banned_str = gconf_client_get_string (firmware->priv->gconf_client, GPK_CONF_BANNED_FIRMWARE, NULL);
@@ -244,17 +257,21 @@ gpk_firmware_remove_banned (GpkFirmware *firmware, GPtrArray *array)
 	/* split using "," */
 	banned = g_strsplit (banned_str, ",", 0);
 
-	/* add to string list */
-	banned_list = egg_str_list_new ();
-	egg_str_list_add_strv (banned_list, banned);
-
-	/* remove all entries in banned list from main list */
-	egg_str_list_remove_list (array, banned_list);
+	/* remove any banned pattern matches */
+	for (i=0; i<array->len; i++) {
+		filename = g_ptr_array_index (array, i);
+		for (j=0; banned[j] != NULL; j++) {
+			ret = g_pattern_match_simple (banned[j], filename);
+			if (ret) {
+				egg_debug ("match %s for %s, removing", banned[j], filename);
+				g_free (filename);
+				g_ptr_array_remove_index_fast (array, i);
+			}
+		}
+	}
 out:
 	g_free (banned_str);
 	g_strfreev (banned);
-	if (banned_list != NULL)
-		g_object_unref (banned_list);
 }
 
 /**
@@ -313,8 +330,8 @@ gpk_firmware_init (GpkFirmware *firmware)
 	GPtrArray *array;
 
 	firmware->priv = GPK_FIRMWARE_GET_PRIVATE (firmware);
-	firmware->priv->array_found = egg_str_list_new ();
-	firmware->priv->array_requested = egg_str_list_new ();
+	firmware->priv->array_found = g_ptr_array_new ();
+	firmware->priv->array_requested = g_ptr_array_new ();
 	firmware->priv->gconf_client = gconf_client_get_default ();
 
 	/* should we check and show the user */
@@ -345,7 +362,7 @@ gpk_firmware_init (GpkFirmware *firmware)
 		/* file already exists */
 		ret = g_file_test (filename_path, G_FILE_TEST_EXISTS);
 		if (!ret)
-			egg_str_list_add (array, filename_path);
+			g_ptr_array_add (array, g_strdup (filename_path));
 
 		g_free (filename_decoded);
 		g_free (filename_path);
@@ -354,12 +371,9 @@ gpk_firmware_init (GpkFirmware *firmware)
 	}
 	g_dir_close (dir);
 
-	/* don't request duplicates */
-	egg_str_list_remove_duplicate (array);
-
 	/* debugging */
 	for (i=0; i<array->len; i++) {
-		filename = egg_str_list_index (array, i);
+		filename = g_ptr_array_index (array, i);
 		egg_debug ("requested: %s", filename);
 	}
 
@@ -368,7 +382,7 @@ gpk_firmware_init (GpkFirmware *firmware)
 
 	/* debugging */
 	for (i=0; i<array->len; i++) {
-		filename = egg_str_list_index (array, i);
+		filename = g_ptr_array_index (array, i);
 		egg_debug ("searching for: %s", filename);
 	}
 
@@ -391,8 +405,10 @@ gpk_firmware_finalize (GObject *object)
 	firmware = GPK_FIRMWARE (object);
 
 	g_return_if_fail (firmware->priv != NULL);
-	egg_str_list_free (firmware->priv->array_found);
-	egg_str_list_free (firmware->priv->array_requested);
+	g_ptr_array_foreach (firmware->priv->array_found, (GFunc) g_free, NULL);
+	g_ptr_array_free (firmware->priv->array_found, TRUE);
+	g_ptr_array_foreach (firmware->priv->array_requested, (GFunc) g_free, NULL);
+	g_ptr_array_free (firmware->priv->array_requested, TRUE);
 	g_object_unref (firmware->priv->gconf_client);
 
 	G_OBJECT_CLASS (gpk_firmware_parent_class)->finalize (object);
