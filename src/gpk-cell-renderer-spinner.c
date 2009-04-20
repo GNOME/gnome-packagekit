98e0a025aa767288fde1e3d82d9e77d4f60b93d0
Richard Hughes <richard@hughsie.com>
2009-04-20 11:28:50 +0100

    Trivial compile fix to the last commit (my mistake)

--- src/gpk-cell-renderer-spinner.c
+++ src/gpk-cell-renderer-spinner.c
@@ -20,7 +20,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#include <gtk/gtk.h>
 #include <gpk-cell-renderer-spinner.h>
+#include "egg-debug.h"
 
 enum {
 	PROP_0,
