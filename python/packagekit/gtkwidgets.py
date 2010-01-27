995d8c939f43dc560aeaa4356a4f7a2431fb8ef5
Richard Hughes <richard@hughsie.com>
2010-01-27 16:28:06 +0000

    Update the Free Software Foundation address

--- python/packagekit/gtkwidgets.py
+++ python/packagekit/gtkwidgets.py
@@ -17,7 +17,7 @@ This module provides widgets to use PackageKit in a GTK application.
 
 #You should have received a copy of the GNU General Public License
 #along with this program; if not, write to the Free Software
-#Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 __author__ = "Sebastian Heinlein <devel@glatzor.de>"
 
