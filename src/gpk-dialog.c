d49cb115fb4e7d6fbd2233fc105b2b95312b4901
William Jon McCann <jmccann@redhat.com>
2013-02-08 08:02:42 -0500

    Don't use horizontal scrollbars
    https://bugzilla.gnome.org/show_bug.cgi?id=693416

--- src/gpk-dialog.c
+++ src/gpk-dialog.c
@@ -250,7 +250,7 @@ gpk_dialog_embed_file_list_widget (GtkDialog *dialog, GPtrArray *files)
 	/* scroll the treeview */
 	scroll = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll),
-					GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+					GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
 	gtk_container_add (GTK_CONTAINER (scroll), widget);
 	gtk_widget_show (scroll);
 
@@ -405,7 +405,7 @@ gpk_dialog_tabbed_package_list_widget (GtkWidget *tab_page, GPtrArray *array)
 
 	/* scroll the treeview */
 	scroll = gtk_scrolled_window_new (NULL, NULL);
-	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
 	gtk_container_add (GTK_CONTAINER (scroll), widget);
 	gtk_widget_show (scroll);
 
