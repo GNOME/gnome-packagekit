81b357a0978bfb1d5f0296f5df46f858fa0191c1
Richard Hughes <richard@hughsie.com>
2013-03-01 14:19:05 +0000

    Do not use the deprecated gtk_scrolled_window_add_with_viewport()

--- src/gpk-dialog.c
+++ src/gpk-dialog.c
@@ -193,7 +193,7 @@ gpk_dialog_embed_package_list_widget (GtkDialog *dialog, GPtrArray *array)
 	/* scroll the treeview */
 	scroll = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scroll), widget);
+	gtk_container_add (GTK_CONTAINER (scroll), widget);
 	gtk_widget_show (scroll);
 
 	/* add some spacing to conform to the GNOME HIG */
@@ -251,7 +251,7 @@ gpk_dialog_embed_file_list_widget (GtkDialog *dialog, GPtrArray *files)
 	scroll = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll),
 					GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scroll), widget);
+	gtk_container_add (GTK_CONTAINER (scroll), widget);
 	gtk_widget_show (scroll);
 
 	/* set in buffer */
@@ -406,7 +406,7 @@ gpk_dialog_tabbed_package_list_widget (GtkWidget *tab_page, GPtrArray *array)
 	/* scroll the treeview */
 	scroll = gtk_scrolled_window_new (NULL, NULL);
 	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (scroll), widget);
+	gtk_container_add (GTK_CONTAINER (scroll), widget);
 	gtk_widget_show (scroll);
 
 	/* add some spacing to conform to the GNOME HIG */
