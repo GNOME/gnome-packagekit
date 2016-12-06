f2f986288d0fba190875217405d27fee4f1ba673
Richard Hughes <richard@hughsie.com>
2016-12-06 19:50:50 +0000

    Port to the meson build system
    Automake and autoconf are impossible to fully understand and Meson now provides
    everything we need for a much smaller, faster, and more understandable build.
    
    See http://mesonbuild.com/ for more information.

---
+++ data/icons/create-application-icons.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo "Starting custom installation step"
+
+if [ -z $MESON_INSTALL_PREFIX ]; then
+    echo 'This is meant to be ran from Meson only!'
+    exit 1
+fi
+
+PKGICONSDIR="${DESTDIR:-}${MESON_INSTALL_PREFIX}/share/gnome-packagekit/icons/hicolor";
+ICONSDIR="${DESTDIR:-}${MESON_INSTALL_PREFIX}/share/icons/hicolor";
+echo ${ICONSDIR}
+mkdir -p ${ICONSDIR}/scalable/apps
+mkdir -p ${ICONSDIR}/scalable/mimetypes
+ln -fs ${PKGICONSDIR}/scalable/status/pk-package-sources.svg \
+    ${ICONSDIR}/scalable/apps/gpk-repo.svg
+ln -fs ${PKGICONSDIR}/scalable/status/pk-package-info.svg \
+    ${ICONSDIR}/scalable/apps/gpk-log.svg
+ln -fs ${PKGICONSDIR}/scalable/status/pk-update-high.svg \
+    ${ICONSDIR}/scalable/apps/gpk-prefs.svg
+ln -fs ${PKGICONSDIR}/scalable/status/pk-package-info.svg \
+    ${ICONSDIR}/scalable/mimetypes/application-x-package-list.svg
+ln -fs ${PKGICONSDIR}/scalable/status/pk-collection-installed.svg \
+    ${ICONSDIR}/scalable/mimetypes/application-x-catalog.svg
+
+echo "Finished custom install step"
